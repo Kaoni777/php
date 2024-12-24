89c0a29a79fd56bc01a859c5095b0d92e4060fd0
Kaoni777 <kaondragaqina@gmail.com>
2024-12-24 19:15:17 +0100

    Add files via upload

---
+++ challenge/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'db.php';
+
+$result = $conn->query("SELECT * FROM products");
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Managment</title>
+    <link href="">
+</head>
+<body class="bg-light">
+    <div class="container mt-5">
+        <h1 class="text centre">Product Managment</h1>
+    
+</body>
+</html>
